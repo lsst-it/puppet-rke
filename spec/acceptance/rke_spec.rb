a5b7258ca8778b4eeeb08aa5c1eea739afb392b6
Joshua Hoblitt <josh@hoblitt.com>
2022-01-20 11:32:06 -0700

    add acceptance tests for version param

--- spec/acceptance/rke_spec.rb
+++ spec/acceptance/rke_spec.rb
@@ -80,4 +80,44 @@ describe 'rke class' do
       it { is_expected.to be_linked_to "#{basedir}/dl/1.3.4/rke_linux-amd64" }
     end
   end
+
+  context 'with version related params' do
+    let(:pp) do
+      <<-EOS
+      class { rke:
+        version  => '1.3.3',
+        checksum => '61088847d80292f305e233b7dff4ac8e47fefdd726e5245052450bf05da844aa',
+      }
+      EOS
+    end
+
+    it_behaves_like 'an idempotent resource'
+
+    %w[
+      /opt/rke
+      /opt/rke/dl
+      /opt/rke/dl/1.3.3
+    ].each do |d|
+      describe file(d) do
+        it { is_expected.to be_directory }
+        it { is_expected.to be_owned_by 'root' }
+        it { is_expected.to be_grouped_into 'root' }
+        it { is_expected.to be_mode '755' }
+      end
+    end
+
+    describe file('/opt/rke/dl/1.3.3/rke_linux-amd64') do
+      it { is_expected.to be_file }
+      it { is_expected.to be_owned_by 'root' }
+      it { is_expected.to be_grouped_into 'root' }
+      it { is_expected.to be_mode '755' }
+    end
+
+    describe file('/usr/bin/rke') do
+      it { is_expected.to be_symlink }
+      it { is_expected.to be_owned_by 'root' }
+      it { is_expected.to be_grouped_into 'root' }
+      it { is_expected.to be_linked_to '/opt/rke/dl/1.3.3/rke_linux-amd64' }
+    end
+  end
 end
